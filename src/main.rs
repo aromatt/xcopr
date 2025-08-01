36f4ef11f557d9849a8641847809af88afc37f22
Andrew Matteson <amatteson3@gmail.com>
2025-07-31 19:47:56 -0500

    Add XCOPR_SHELL environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process;
+use std::env;
 use std::process::Child;
 use std::process::Stdio;
 use std::fmt;
@@ -21,6 +22,7 @@ pub enum XcoprError {
     },
     StdoutNotCaptured(String),
     MissingArgs(&'static str),
+    BadShell(String),
 }
 
 impl fmt::Display for XcoprError {
@@ -38,12 +40,12 @@ impl fmt::Display for XcoprError {
             }
             MissingArgs(arg) => write!(f, "missing required argument: {}", arg),
             StdoutNotCaptured(cmd) => write!(f, "stdout not captured for `{}`", cmd),
+            BadShell(cmd) => write!(f, "bad shell: `{}`", cmd),
         }
     }
 }
 
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -51,32 +53,63 @@ struct Args {
     #[arg(short, long)]
     coproc: Vec<String>,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    stream: u8,
+    /// A stream template
+    #[arg(short, long)]
+    stream: Vec<String>,
 }
 
-fn spawn(cmd_str: &str, stdin: Stdio, stdout: Stdio) -> Result<Child, XcoprError> {
-    std::process::Command::new("sh")
-        .arg("-eu")
-        .arg("-c")
-        .arg(cmd_str)
-        .stdin(stdin)
-        .stdout(stdout)
-        .spawn()
-        .map_err(|e| XcoprError::SpawnFailed {
-            command: cmd_str.to_string(),
-            source: e,
+#[derive(Debug)]
+struct Shell {
+    program: String,
+    args: Vec<String>,
+}
+
+impl Shell {
+
+    fn from_str(cmd_str: &str) -> Result<Shell, XcoprError> {
+        let mut iter = cmd_str.split_whitespace();
+        let program = iter.next().take().ok_or_else(|| {
+            XcoprError::BadShell(cmd_str.to_string())
+        })?;
+        let args = iter.map(|s| s.to_string()).collect();
+        Ok(Shell {
+            program: program.to_string(),
+            args,
         })
+    }
+
+    fn from_env(var_name: &str, default: &str) -> Result<Shell, XcoprError> {
+        env::var(var_name)
+            .or(Ok(default.to_string()))
+            .map(|s| Shell::from_str(&s))?
+    }
+
+    fn full_cmd_str(&self, cmd: &str) -> String {
+        format!("{} {} '{}'", self.program, self.args.join(" "), cmd)
+    }
+
+    fn spawn(&self, cmd_str: &str, stdin: Stdio, stdout: Stdio) -> Result<Child, XcoprError> {
+        std::process::Command::new(&self.program)
+            .args(&self.args)
+            .arg(cmd_str)
+            .stdin(stdin)
+            .stdout(stdout)
+            .spawn()
+            .map_err(|e| XcoprError::SpawnFailed {
+                command: self.full_cmd_str(cmd_str),
+                source: e,
+            })
+    }
 }
 
 fn run(args: Args) -> Result<(), XcoprError> {
     let mut children = Vec::new();
     let mut next_stdin: Stdio = Stdio::inherit();
+    let shell = Shell::from_env("XCOPR_SHELL", "sh -euc")?;
 
     // Handle all but the last command
     for cmd_str in &args.coproc[..args.coproc.len().saturating_sub(1)] {
-        let mut child = spawn(cmd_str, next_stdin, Stdio::piped())?;
+        let mut child = shell.spawn(cmd_str, next_stdin, Stdio::piped())?;
 
         let stdout = child.stdout.take().ok_or_else(|| {
             XcoprError::StdoutNotCaptured(cmd_str.to_string())
@@ -88,7 +121,7 @@ fn run(args: Args) -> Result<(), XcoprError> {
 
     // Handle last command, which inherits stdout from xcopr
     if let Some(cmd_str) = args.coproc.last() {
-        let child = spawn(cmd_str, next_stdin, Stdio::inherit())?;
+        let child = shell.spawn(cmd_str, next_stdin, Stdio::inherit())?;
         children.push((cmd_str, child));
     }
 
